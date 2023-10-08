3227b4861d96e8d838be4f63220274fffff99c8f
Brahim-BT <baibbat.brahim@gmail.com>
2023-10-08 17:53:54 +0100

    Resolving

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for element in list:
+            if element != 0:
+                print(" ", end="")
             print("{:d}".format(element), " ", end="")
         print()
